6f893eb1ee32774174e6c9765895e57d83fe189b
root <root@ip-172-31-44-189.us-east-2.compute.internal>
2019-09-17 07:26:39 +0000

    First commit

---
+++ eci.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-02f706d959cedf89"
+  instance_type = "t2.micro"
+  key_name      = "Mykeypair100"
+}
