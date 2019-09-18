b0b41b48098d749f63bbfbf509ae78e119312211
Abhishek247 <37483243+Abhishek247@users.noreply.github.com>
2019-09-18 15:16:17 +0530

    Update eci.tf

--- eci.tf
+++ eci.tf
@@ -7,4 +7,7 @@ resource "aws_instance" "example" {
   ami           = "ami-08af61eccb1ae7849"
   instance_type = "t2.micro"
   key_name      = "Mykeypair100"
+  tags{
+	Name = "MyEC2Instance"
+	}
 }
